41d48d113bdf2307749a1bd026e12c67e77e4618
Ebrar <16155052@mersin.edu.tr>
2023-03-15 15:32:10 +0300

    findby queryby

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { useState,useEffect } from 'react';
 
 function TestComponent()  {
+  const [message, setMessage] = useState('Modern Testing')
+  useEffect(() => {
+    setTimeout(() => {
+      setMessage('Modern Testing with React')
+    }, 1000)
+  }, [])
+
   return <> 
- Modern test
+    <p>{message}</p>
         </>
 }
 
-test("should render the element correctly",() =>{
+test("should render the element correctly",  () =>{
   render (<TestComponent/>)
-  const element = screen.getByText('modern',{
-    exact: false,
-  })
-  expect(element).toBeInTheDocument();
+  const element =  screen.queryByText('modern')
+  expect(element).not.toBeInTheDocument();
 })
