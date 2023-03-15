20f2b5215edfdafec4e9c19a593c4ba26e139ca2
Ebrar <16155052@mersin.edu.tr>
2023-03-15 12:40:33 +0300

    getByLabel

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,14 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 function TestComponent()  {
-  return <> <button>Click Me</button>
-        <h1>ebrarmeaag</h1>
+  return <> 
+  <label for="user-name">User name:</label>
+  <input type="text" id="user-name" />
         </>
 }
 
 test("should render the element correctly",() =>{
   render (<TestComponent/>)
-  const element = screen.getByRole('button')
-  expect(element).toBeInTheDocument();
-})
-test (" should render the h1 correctly",()=>{
-  render(<TestComponent/>)
-  const element = screen.getByRole('heading',{
-    name:"ebrarmeaag",
-  })
+  const element = screen.getByLabelText('User name:')
   expect(element).toBeInTheDocument();
 })
