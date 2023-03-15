6d9d53000915b6d43c1085c9b27a91013c9cfcd7
Ebrar <16155052@mersin.edu.tr>
2023-03-15 19:44:52 +0300

    domquery

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,19 @@
 import { waitFor,render,waitForElementToBeRemoved, screen } from '@testing-library/react';
 import App from './App';
-import { useState,useEffect } from 'react';
 
-function TestComponent()  {
-  const [message, setMessage] = useState('Modern Testing')
-  useEffect(() => {
-    setTimeout(() => {
-      setMessage('Modern Testing with React')
-    }, 1000)
-  }, [])
 
-  return <> 
-    <p>{message}</p>
-        </>
+function TestComponent() {
+  return ( 
+    <div>
+      <p className='primary-text'>tantuni kebab</p>
+    </div>
+ 
+
+
+  );
 }
 
-test("should render the element correctly", async () =>{
-  render (<TestComponent/>)
-
-  await waitFor(() => {
-    expect(screen.getByText('Modern Testing with React')).toBeInTheDocument()
-
-  }
-  )
-  await waitForElementToBeRemoved(() => { screen.queryByText("Modern Testing") })
-})
+test('should render the component', () => {
+  const {container} = render(<TestComponent/>);
+  const element = container.querySelector('.primary-text');
+});
