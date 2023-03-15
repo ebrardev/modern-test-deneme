4498238569c7dc628ebcc03de3d76fa697349918
Ebrar <16155052@mersin.edu.tr>
2023-03-15 13:33:18 +0300

    getByPlaceHolder

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import App from './App';
 
 function TestComponent()  {
   return <> 
-  <label for="user-name">User name:</label>
-  <input type="text" id="user-name" />
+
+  <input placeholder='ula ekrem' type="text" id="user-name" />
         </>
 }
 
 test("should render the element correctly",() =>{
   render (<TestComponent/>)
-  const element = screen.getByLabelText('User name:')
+  const element = screen.getByPlaceholderText('ula ekrem')
   expect(element).toBeInTheDocument();
 })
