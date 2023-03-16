2c86c724cb4dce51649597985908c516b8afa278
Ebrar <16155052@mersin.edu.tr>
2023-03-16 11:25:44 +0300

    clipboard events

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render,screen } from "@testing-library/react";
 import { useState } from "react";
 import userEvent from "@testing-library/user-event";
+
 function TestComponent() {
   const [count, setCount] = useState(0);
   const handleIncrement = () =>  {
@@ -9,22 +10,41 @@ function TestComponent() {
  
 };
 
+const handleCopy = () => {
+  window.navigator.clipboard.writeText("count");
+};
 return (
   <div>
     <h1>{count}</h1>
-    <button onClick={handleIncrement}>Increment</button>
+    <button onClick={handleCopy}>Copy</button>
+     <input type="text" value={count} onChange={handleIncrement} />
   </div>
 )
 };
 
+// it('should increment the count',  async () => {
+//   const user = userEvent.setup();
+//   render(<TestComponent />);
+//   const buttonElement = screen.getByRole('button'); 
+
+//  const headingElement = screen.getByRole('heading');
+//   await user.keyboard('[tab]');
+//   await user.keyboard('[Enter]');
+//  expect(buttonElement).toHaveFocus();
+//  expect(headingElement).toHaveTextContent('1');
+// })
+
 it('should increment the count',  async () => {
   const user = userEvent.setup();
   render(<TestComponent />);
-  const buttonElement = screen.getByRole('button'); 
 
- const headingElement = screen.getByRole('heading');
+  const inputElement = screen.getByRole('textbox');
   await user.keyboard('[tab]');
-  await user.keyboard('[Enter]');
- expect(buttonElement).toHaveFocus();
- expect(headingElement).toHaveTextContent('1');
+ await user.keyboard('[Enter]');
+//  await user.keyboard('[tab]');   
+    // inputElement.focus();
+ inputElement.focus();
+ await user.paste();
+
+ expect(inputElement).toHaveValue("count")
 })
